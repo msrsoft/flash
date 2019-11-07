adbbf95f6322f35dbcf63f1a2f75e3fd7cb12940
Your Name <you@example.com>
2019-11-07 00:52:28 -0800

    updated maven project

---
+++ Moc/src/main/java/com/subhani/Moc/App.java
@@ -0,0 +1,13 @@
+package com.subhani.Moc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
